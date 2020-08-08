cd5ad2f0cbcb1e201a43db37a6926b0583ac4fe6
marcfreir <marcfreir@outlook.com>
2020-08-08 07:43:53 -0200

    Update class Animation

--- SvartSlott/src/com/marc/entity/Animation.java
+++ SvartSlott/src/com/marc/entity/Animation.java
@@ -1,5 +1,63 @@
 package com.marc.entity;
 
+import java.awt.image.BufferedImage;
+
 public class Animation
 {
+    private BufferedImage[] frames;
+    private int currentFrame;
+
+    private long startTime;
+    private long delay;
+
+    private boolean playedOnce;
+
+    //Constructor
+    public void Animation()
+    {
+        playedOnce = false;
+    }
+
+    //Setters
+    public void setFrames(BufferedImage[] frames)
+    {
+        this.frames = frames;
+        currentFrame = 0;
+        startTime = System.nanoTime();
+        playedOnce = false;
+    }
+
+    public void setDelay(long delay)
+    {
+        this.delay = delay;
+    }
+
+    public void setCurrentFrame(int currentFrame)
+    {
+        this.currentFrame = currentFrame;
+    }
+
+    public void updateAnimation()
+    {
+        if (delay == -1)
+        {
+            return;
+        }
+
+        long elapsed = (System.nanoTime() - startTime) / 1000000;
+
+        if (elapsed > delay)
+        {
+            currentFrame++;
+            startTime = System.nanoTime();
+        }
+
+        if (currentFrame == frames.length)
+        {
+            currentFrame = 0;
+            playedOnce = true;
+        }
+    }
+
+    
 }
