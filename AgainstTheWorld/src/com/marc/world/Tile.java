dedca6216ac122c356fc887def8134078f824127
marcfreir <marcfreir@outlook.com>
2020-08-22 14:03:41 -0200

    Update file spriteSheet.png and TILE_SPACE_BARRIER width in class Tile

--- AgainstTheWorld/src/com/marc/world/Tile.java
+++ AgainstTheWorld/src/com/marc/world/Tile.java
@@ -8,7 +8,7 @@ import java.awt.image.BufferedImage;
 public class Tile {
 
     public static BufferedImage TILE_SPACE_BACKGROUND = Game.spriteSheet.getSpritesheet(200, 160, 40, 40);
-    public static BufferedImage TILE_SPACE_BARRIER = Game.spriteSheet.getSpritesheet(280, 160, 80, 40);
+    public static BufferedImage TILE_SPACE_BARRIER = Game.spriteSheet.getSpritesheet(280, 160, 40, 40);
 
     private BufferedImage tileSprite;
     private int tileAxisX;
